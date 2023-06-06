e730921fa27d2da9db0f5b55fa037b54fbc73a88
Bilalkhan123456789 <bk315818@gmail.com>
2023-06-06 18:05:24 +0500

    changing

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,26 +3,31 @@ import { Link } from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Images from '../Images/logo.png'
+import { BsSearch } from 'react-icons/bs';
+
+
 
 
 function Home() {
   return (
     < >
-      <div >
-        <Navbar bg="warning" expand="lg">
+      <div>
+        <Navbar bg="smoke" expand="lg">
           <Container fluid>
             <Navbar.Brand href="#"><Link to="/"><img src={Images} alt="" style={{ height: "40px" }} /></Link></Navbar.Brand>
+      
+
             <Navbar.Toggle aria-controls="navbarScroll" />
             <Navbar.Collapse id="navbarScroll">
-              <Nav
-                className="me-auto my-2 my-lg-0"
-                style={{ maxHeight: '100px', marginLeft: "250px" }}
-                navbarScroll>
-                <Nav.Link href="#action1" ><Link to="/" style={{ textDecoration: "none", color: "black" }}>Home</Link></Nav.Link>
-                <Nav.Link href="#action2"><Link to="/gallery" style={{ textDecoration: "none", color: "black" }}>Gallery</Link></Nav.Link>
-                <Nav.Link href="#" ><Link to="/service" style={{ textDecoration: "none", color: "black" }}>Services</Link></Nav.Link>
-                <Nav.Link href="#action2"><Link to="/about" style={{ textDecoration: "none", color: "black" }}>About</Link></Nav.Link>
-                <Nav.Link href="#action2"><Link to="/login" style={{ textDecoration: "none", color: "black" }}>Contact us</Link></Nav.Link>
+           
+
+
+               
+              <Nav className="me-auto my-2 my-lg-0"style={{  display:"flex",justifyContent:"end",marginLeft:"700px" }} navbarScroll>
+                <Nav.Link href="#action1" ><Link to="/" style={{ textDecoration: "none", color: "black"   }}><BsSearch/></Link></Nav.Link>
+                <Nav.Link href="#action2"><Link to="/gallery" style={{ textDecoration: "none", color: "black" }}>WishList</Link></Nav.Link>
+                <Nav.Link href="#" ><Link to="/service" style={{ textDecoration: "none", color: "black" }}>Cart</Link></Nav.Link>
+                <Nav.Link href="#action2"><Link to="/about" style={{ textDecoration: "none", color: "black" }}>Account</Link></Nav.Link>
 
 
               </Nav>
--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,29 +1,36 @@
 import React from "react";
 import img1 from "../Images/Slider image 1.jpg"
 import img2 from "../Images/slider image 2.jpg"
-import img3 from "../Images/slider image 3.jpg"
-
-import img21 from "../Images/15.jpg"
+import Carousel from 'react-bootstrap/Carousel';
+import img17 from "../Images/17.jpg"
 import img20 from "../Images/20.jpg"
 import img19 from "../Images/19.jpg"
 import img18 from "../Images/18.jpg"
-import img17 from "../Images/17.jpg"
-import img16 from "../Images/16.jpg"
+import img01 from "../Images/1.png"
+import img02 from "../Images/2.jpg"
+import img3 from "../Images/3.png"
+import img4 from "../Images/4.png"
+import aboutimg from '../Images/About.jpg'
+import lastpic from '../Images/LastPic.jpg'
 
 
 
+import AOS from "aos";
+import 'aos/dist/aos.css';
 
 
-import Carousel from 'react-bootstrap/Carousel';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
 
 
 
 class Slider extends React.Component{
+  componentDidMount() {
+    AOS.init();
+  }
 render(){
     return(
         <>
+
+{/* Slider */}
    <Carousel>
       <Carousel.Item  >
         <img style={{maxHeight:"600px"}}
@@ -39,125 +46,104 @@ render(){
           src={img2}
           alt="Second slide"
         />
-
-        
-      </Carousel.Item>
-      <Carousel.Item>
-        <img style={{maxHeight:"600px"}}
-          className="d-block w-100"
-          src={img3}
-          alt="Third slide"
-        />
-
-         
       </Carousel.Item>
     </Carousel>
 
 
 
+{/* Cards second part */}
 
-
-
-{/* Animation Cards */}
-
-
-      
-
-
-    <div className="container" style={{ marginTop: "50px" }}>
-          <div class="row">
-            <div className="col" lg-3 md-4 sm-6 col-12>
-            <Card style={{ width: '18rem'}}>
-      <Card.Img variant="top" src={img21} />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+<div className="container-fluid text-center" style={{backgroundColor:"black",marginTop:"30px"}}>
+  <div className="row">
+    <h1 style={{textAlign:"center" , fontFamily:"serif" , marginTop:"10px" , color:"gold"}}>SHOP BY CATEGORY</h1>
+ 
+<div className="col" col-lg-3 md-4 sm-6 col-12>
+<div data-aos="fade-right">
+<img style={{borderRadius:"30px" ,width:"310px"  ,height:"400px"}} src={img17} alt="" />
+ </div>
 </div>
 
 
-<div className="col" lg-3 md-4 sm-6 col-12>
-<Card style={{ width: '18rem'}}>
-      <Card.Img variant="top" src={img20} />
-      <Card.Body >
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+ <div className="col" col-lg-3 md-4 sm-6 col-12>
+ <div data-aos="fade-right">
+<img style={{borderRadius:"30px" , width:"310px"  ,  height:"400px"}} src={img20} alt="" />
+ </div> </div>
+
+
+ <div className="col" col-lg-3 md-4 sm-6 col-12>
+ <div data-aos="fade-left">
+
+<img style={{borderRadius:"30px" , width:"310px"  , height:"400px"}} src={img19} alt="" />
+ </div> </div>
+
+
+ <div className="col" col-lg-3 md-4 sm-6 col-12>
+ <div data-aos="fade-left">
+
+<img style={{borderRadius:"30px" , width:"310px"  , height:"400px"}} src={img18} alt="" />
+ </div> </div>
+  </div>
+  <br /> <br /> 
 </div>
 
-<div className="col" lg-3 md-4 sm-6 col-12>
-<Card style={{ width: '18rem'}}>
-      <Card.Img variant="top" src={img19} />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+{/*  Third Part */}
+
+    <div className="container-fluid text-center" style={{backgroundColor:"whitesmoke"}}>
+  <div className="row">
+    <h1 style={{textAlign:"center" , fontFamily:"serif" , marginTop:"10px" , color:"black"}}>BEST SELLERS</h1>
+   
+
+    <div className="col" col-lg-3 md-4 sm-6 col-12>
+    <div class="shadow">
+    <div data-aos="flip-up">
+<img style={{borderRadius:"10px" , width:"300px"  ,  height:"300px"}} src={img01} alt="" />
+ </div> </div>    </div>
+
+
+ <div className="col" col-lg-3 md-4 sm-6 col-12>
+ <div class="shadow  ">
+ <div data-aos="flip-up">
+<img style={{borderRadius:"10px" , width:"300px"  ,  height:"300px"}} src={img02} alt="" />
+ </div> </div> </div>
+
+ <div className="col" col-lg-3 md-4 sm-6 col-12>
+ <div class="shadow  ">
+ <div data-aos="flip-up">
+<img style={{borderRadius:"10px" , width:"300px"  ,  height:"300px"}} src={img3} alt="" />
+ </div> </div> </div>
+
+ <div className="col" col-lg-3 md-4 sm-6 col-12>
+ <div class="shadow ">
+ <div data-aos="flip-up">
+<img style={{borderRadius:"10px" , width:"300px"  ,  height:"300px"}} src={img4} alt="" />
+ </div> </div> </div>
+</div></div>
+
+{/* About Section */}
+
+<div className="container-fluid text-center" style={{backgroundColor:"black",marginTop:"30px"}}>
+  <div className="row">
+<div className="col" style={{marginTop:"30px"}}>
+<h1 style={{color:"gold", fontFamily:"fantasy"}} >ABOUT US</h1>
+<p style={{color:"white " , textAlign:"center" }}>
+kBH established itself as a trusted source of unique footwear as we proudly announce our products as “Made in Pakistan ''. 
+  KBH dedicates itself to quality, unique design, and utmost comfort. Come join the spirit of ultimate
+  fashion footwear to indulge yourself with uniqueness & sophistication which is constructed with high-quality craftsmanship.</p>
+  <img src={aboutimg} alt="" style={{width:"100%"}} />
 </div>
 
-<div className="col" lg-3 md-4 sm-6 col-12 style={{marginTop:"20px"}}> 
-<Card style={{ width: '18rem'}}>
-      <Card.Img variant="top" src={img18} />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+  </div>
 </div>
 
+{/* forth Part */}
+<div className="container-fluid " >
+  <div className="row">
+    <div className="col" style={{marginTop:"20px" , fontWeight:"bolder"}}>
+<img src={lastpic} style={{width:"100%"}} alt="" />
 
-<div className="col" lg-3 md-4 sm-6 col-12 style={{marginTop:"20px"}}> 
-<Card style={{ width: '18rem'}}>
-      <Card.Img variant="top" src={img17} />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-</div>
-
-
-<div className="col" lg-3 md-4 sm-6 col-12 style={{marginTop:"20px"}}> 
-<Card style={{ width: '18rem'}}>
-      <Card.Img variant="top" src={img16} />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-</div>
-
-
-
-</div>
-</div>
-
+    </div>
+    </div>
+    </div>
         </>
     )
 }
